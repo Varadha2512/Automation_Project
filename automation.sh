404664ab8b5def786930193f4edc17bf446ffe2a
Varadha2512 <93592049+Varadha2512@users.noreply.github.com>
2021-11-30 19:13:05 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-sudo apt update
-sudo apt-get install apache2
+
+echo -e "\n\nUpdating Apt Packages and upgrading latest patches\n"
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+echo -e "\n\nInstalling Apache2 Web server\n"
+sudo apt-get install apache2 -y
+
+echo -e "\n\nRestarting Apache\n"
 sudo service apache2 restart
-sudo systemctl status apache2.service
+
+echo -e "\n\nAPACHE2 Installation Completed"
+sudo systemctl status apache2
+
+echo "TAR file creation for apache2 logs"
 d=$(date +%Y-%m-%d-%H%M%S)
 SOURCE="/var/log/apache2/"
 sudo tar -cvzpf varadha-httpd-logs-$d.tar $SOURCE
 sudo mv varadha-httpd-logs-$d.tar /tmp
+
+echo "Uploading TAR file to AWS S3 bucket"
 aws s3 \
 cp /tmp/varadha-httpd-logs-$d.tar \
 s3://upgrad-varadharajan/varadha-httpd-logs-$d.tar
+
+exit 0
